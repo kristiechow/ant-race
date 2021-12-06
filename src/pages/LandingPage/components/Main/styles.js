033efca6f17d63539c3cc41ff7a8c5d3168fbc2d
Kristie Chow <kristiemhchow@gmail.com>
2021-12-06 09:27:33 +0700

    Update styles

--- src/pages/LandingPage/components/Main/styles.js
+++ src/pages/LandingPage/components/Main/styles.js
@@ -1,17 +1,10 @@
 import styled from "styled-components";
 
 export const LightText = styled.h1`
-  font-size: 30px;
+  font-size: 16px;
   line-height: 33px;
-  color: #333333;
-`;
-
-export const HeavyText = styled.h1`
-  font-weight: bold;
-  font-size: 52px;
-  line-height: 57px;
-  display: flex;
-  align-items: center;
+  color: #6B6B6B;
+  font-weight: light;
 `;
 
 export const Picture = styled.img`
